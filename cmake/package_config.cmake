62ecb0aac18bc5334fff3ec405c6bb685e330117
Ben Vining <ben.the.vining@gmail.com>
2022-12-08 15:46:57 -0600

    initial commit of tests

--- cmake/package_config.cmake
+++ cmake/package_config.cmake
@@ -1,12 +1,12 @@
 @PACKAGE_INIT@
 
-find_dependency (JUCE 7.0.3)
+include (CMakeFindDependencyMacro)
+
+find_dependency (JUCE)
 
 include ("${CMAKE_CURRENT_LIST_DIR}/PerfettoTargets.cmake")
 
-set_and_check (module_path "@PACKAGE_MP_INSTALL_DEST@/melatonin_perfetto")
-
-juce_add_module ("${module_path}"
+juce_add_module ("${CMAKE_CURRENT_LIST_DIR}/melatonin_perfetto"
 				 ALIAS_NAMESPACE Melatonin)
 
 target_link_libraries (melatonin_perfetto INTERFACE perfetto::perfetto)
